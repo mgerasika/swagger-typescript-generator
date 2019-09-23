19a50fa1d7917f9bd4d208668a9866aafe22a3de
Mykhaylo Herasika <mykhaylo.herasika@globaltechmakers.com>
2019-09-23 16:54:18 +0300

    refactoring code

--- dist/react-app/dist/swagger/components/api-class/api-class-constructor.d.ts
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { SwaggerClassModel } from "../../model/swagger-class";
-interface IProps {
-    swaggerClass: SwaggerClassModel;
-}
-export declare const ApiClassConstructorComponent: React.FC<IProps>;
-export {};
--- dist/react-app/dist/swagger/components/index.d.ts
+++
@@ -1,3 +0,0 @@
-export * from './api-class';
-export * from './definitions';
-export * from './api-method';
