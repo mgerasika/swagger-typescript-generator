342f311ab1bb5c2a4acd80eb410f42b0f122c74d
Mykhaylo Herasika <mykhaylo.herasika@globaltechmakers.com>
2019-09-23 16:44:20 +0300

    refactoring code

--- dist/common/swagger-config.d.ts
+++
@@ -1,7 +0,0 @@
-import { ISwaggerPlugin } from "../react-app/src/swagger/common";
-export interface ISwaggerConfig {
-    plugin: ISwaggerPlugin;
-    swaggerInputJson: Object;
-    modelFilesOutDir: string;
-    apiFilesOutDir: string;
-}
