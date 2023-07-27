244ac7c719bd84f1dbfb979c42744b24bb1dc8eb
Mykhaylo Gerasika <mgerasika@gmail.com>
2023-07-27 21:08:55 +0300

    Add firebase

--- src/react-app/src/demo/index.tsx
+++ src/react-app/src/demo/index.tsx
@@ -11,18 +11,18 @@ export * from './swagger-demo-component';
 export * from './bootstrap-panel';
 
 const apiUrls = [
-    'http://localhost:8080/v2/api-docs',
-    'http://localhost:5003/api-json',
-    'http://localhost:3000/api/doc',
+    // 'http://localhost:8080/v2/api-docs',
+    // 'http://localhost:5003/api-json',
+    // 'http://localhost:3000/api/doc',
     'https://petstore.swagger.io/v2/swagger.json',
-    'https://flipdish-yellow-team.azurewebsites.net/swagger/docs/private-v1.0',
-    'https://flipdish-yellow-team.azurewebsites.net/swagger/docs/v1.0',
-    'https://flipdish-green-team.azurewebsites.net/swagger/docs/private-v1.0',
-    'https://flipdish-green-team.azurewebsites.net/swagger/docs/v1.0',
-    'https://flipdish-blue-team.azurewebsites.net/swagger/docs/private-v1.0',
-    'https://flipdish-blue-team.azurewebsites.net/swagger/docs/v1.0',
-    'https://flipdish-orange-team.azurewebsites.net/swagger/docs/private-v1.0',
-    'https://flipdish-orange-team.azurewebsites.net/swagger/docs/v1.0',
+    // 'https://flipdish-yellow-team.azurewebsites.net/swagger/docs/private-v1.0',
+    // 'https://flipdish-yellow-team.azurewebsites.net/swagger/docs/v1.0',
+    // 'https://flipdish-green-team.azurewebsites.net/swagger/docs/private-v1.0',
+    // 'https://flipdish-green-team.azurewebsites.net/swagger/docs/v1.0',
+    // 'https://flipdish-blue-team.azurewebsites.net/swagger/docs/private-v1.0',
+    // 'https://flipdish-blue-team.azurewebsites.net/swagger/docs/v1.0',
+    // 'https://flipdish-orange-team.azurewebsites.net/swagger/docs/private-v1.0',
+    // 'https://flipdish-orange-team.azurewebsites.net/swagger/docs/v1.0',
 ];
 
 export const ExampleComponent: React.FC = () => {
